e59c4a296b1def6fdc2881cfd2cdc395911eec0b
adithyanmanayil <115716906+adithyanmanayil@users.noreply.github.com>
2025-01-05 10:49:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,4 +29,5 @@ def home():
 	return render_template('home.html', data = species)
 
 if __name__ == "__main__":
-	app.run(debug = True)
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host="0.0.0.0", port=port)
